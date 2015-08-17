81f22f51ddc3a1a27bdcd93be0c1ab509b48ffd8
yasemin <godot85@gmail.com>
2015-08-17 13:50:31 +0300

    config file fixed

---
+++ VNGRSCHALLENGE/src/ReadConfigFile.java
@@ -0,0 +1,37 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ReadConfigFile {
+
+	ReadConfigFile(){
+		
+	}
+	
+	public String getProperty(String property) throws IOException {
+		Properties prop = new Properties();
+		InputStream input = null;
+
+		try {
+			input = new FileInputStream("config.properties");
+
+			// load a properties file
+			prop.load(input);
+
+			// get the property value and print it out
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		return prop.getProperty(property);
+	}
+}
